57fb814323ee5ad33eea6e8dfe40a2be06f8ce4b
j8298c <juliocmojica@gmail.com>
2020-02-02 12:00:15 -0500

    Proof of work init

--- BlockChain/block.js
+++ BlockChain/block.js
@@ -1,11 +1,12 @@
 const SHA2565 = require('crypto-js/sha256');
-
+const { DIFFICULTY } = require('../config');
 class Block {
-  constructor(timestamp, lasthash, hash, data) {
+  constructor(timestamp, lasthash, hash, data, nonce) {
     this.timestamp = timestamp;
     this.lasthash = lasthash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
   }
 
   toString() {
@@ -13,33 +14,39 @@ class Block {
     Timestamp: ${this.timestamp}
     Last Hash: ${this.lasthash.substring(0, 10)}
     Hash     : ${this.hash.substring(0, 10)}
+    Nonce    : ${this.nonce}
     Data     : ${this.data}`;
   }
 
   // static gives the ability to call method with creating block
   // instance
   static genesis() {
-    return new this('Genesis time', '------', 'f1r57-h45h', [])
+    return new this('Genesis time', '------', 'f1r57-h45h', [], 0)
   }
 
   static mineBlock(lastBlock, data) {
-    const timestamp = Date.now();
-
+    let hash, timestamp;
     const lastHash = lastBlock.hash;
 
-    const hash = Block.hash(timestamp, lastHash, data);
+    let nonce = 0
 
-    return new this(timestamp, lastHash, hash, data);
+    do{
+      nonce++
+      timestamp = Date.now();
+      hash = Block.hash(timestamp, lastHash, data, nonce);
+    } while(hash.substr(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY))
+    
+    return new this(timestamp, lastHash, hash, data, nonce);
   }
 
-  static hash(timestamp, lastHash, data) {
-    return SHA2565(`${timestamp}${lastHash}${data}`).toString();
+  static hash(timestamp, lastHash, data, nonce) {
+    return SHA2565(`${timestamp}${lastHash}${data}${nonce}`).toString();
   }
 
   static blockHash(block) {
-    const { timestamp, lasthash, data } = block;
+    const { timestamp, lasthash, data, nonce } = block;
 
-    return this.hash(timestamp, lasthash, data)
+    return this.hash(timestamp, lasthash, data, nonce)
   }
 
 }
--- BlockChain/block.test.js
+++ BlockChain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
   let data;
@@ -17,5 +18,9 @@ describe('Block', () => {
 
   it('block should have lastblock hash', () => {
     expect(block.data).toEqual(data)
+  })
+
+  it('generates a `hash` that matches difficulty', () => {
+    expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY))
   })
 })
