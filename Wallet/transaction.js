2a186dc7bda2a0d487835fea9d079c28407e0045
j8298c <juliocmojica@gmail.com>
2020-02-03 05:56:57 -0500

    Transaction class init

--- Wallet/transaction.js
+++ Wallet/transaction.js
@@ -1,27 +1,7 @@
-const ChainUtil = require('../chain.utils')
+const ChainUtils = require('../chain.utils');
 
 class Transaction {
-  constructor() {
-    this.id = ChainUtil.id();
-    this.input = null;
-    this.outputs = [];
-  }
 
-  static newTransaction(senderWallet, recipient, amount) {
-    const transaction = new this();
-
-    if(senderWallet.balance < amount) {
-      console.log(`Amount ${amount} exceeds balance`)
-      return
-    }
-
-    transaction.outputs.push(...[
-      { amount: senderWallet - balance, address: senderWallet.publicKey },
-      { amount, address: recipient }
-    ])
-
-    return transaction
-  }
 }
 
 module.exports = Transaction;
