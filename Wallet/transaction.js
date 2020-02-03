7e4e16330546307bccac9627187a31381e2f82db
j8298c <juliocmojica@gmail.com>
2020-02-03 06:17:57 -0500

    Transactions test

--- Wallet/transaction.js
+++ Wallet/transaction.js
@@ -1,6 +1,29 @@
 const ChainUtils = require('../chain.utils');
 
 class Transaction {
+  constructor() {
+    this.id = ChainUtils.id();
+    this.input = null;
+    this.outputs = [];
+  }
+
+  static newTransaction(senderWallet, recipient, amount) {
+    const transaction = new this();
+
+    if(amount > senderWallet.balance) {
+      console.log(`Amount ${amount} exceeds current balance`)
+      return;
+    }
+
+    transaction.outputs.push(...[
+      { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
+      { amount, address: recipient }
+    ])
+
+    return transaction;
+  }
+
+
 
 }
 
--- Wallet/transaction.test.js
+++ Wallet/transaction.test.js
@@ -1,19 +1,28 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 
-describe('Transaction', () => {
+describe('Trasnsaction', () => {
   let transaction, wallet, recipient, amount;
 
   beforeEach(() => {
-    wallet = new Wallet();
+    wallet = new Wallet;
     amount = 50;
-    recipient = 'recipient1';
+    recipient = 'recipient1'
+
     transaction = Transaction.newTransaction(wallet, recipient, amount);
   })
 
-  it('ouputs the `amount` subtracted from the wallet baclance', () => {
-    console.log(transaction)
-    expect(true).toEqual(true)
+  it('outputs the `amount` sub from wallet balance', () => {
+    expect(
+      transaction.outputs.find(output => output.address === wallet.publicKey).amount
+      ).toEqual(wallet.balance - amount)
   })
+
+  it('outputs the `amount` added to the recipient', () => {
+    expect(
+      transaction.outputs.find(output => output.address === recipient).amount
+      ).toEqual(amount)
+  })
+
 
 })
