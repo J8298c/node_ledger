019c16ad48d1a085cd0fa7f542312872fa68862b
j8298c <juliocmojica@gmail.com>
2020-02-05 20:38:26 -0500

    Check and create new transaction

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const BlockChain = require('../BlockChain');
 const P2PServer = require('./p2p-server');
+const Wallet = require('../Wallet');
+const TransactionPool = require('../Wallet/transaction-pool');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,10 +13,24 @@ app.use(bodyParser.json({ limit: '50mb'}))
 
 
 const bc = new BlockChain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2PServer(bc)
 
 app.get('/blocks', (req, res) => {
   res.json(bc.chain)
+})
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+})
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body;
+
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+
+  res.redirect('/transactions')
 })
 
 
